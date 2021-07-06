7b72b4f8104f18109518970fa75d6198ee504208
Henry Choo <lchoo9294@gmail.com>
2021-07-06 22:26:03 +0800

    refactor: reorder links

--- src/links.ts
+++ src/links.ts
@@ -1,6 +1,6 @@
 export const websiteLinks = [
-  'https://s.weibo.com/weibo/everrise?topnav=1&wvr=6&b=1',
   'https://www.dextools.io/app/pancakeswap/pair-explorer/0x93d94fcb0dcc8a88257b2d2eec7a2615ebedb542',
   'https://coinmarketcap.com/currencies/everrise/',
   'https://www.coingecko.com/en/coins/everrise',
+  'https://s.weibo.com/weibo/everrise?topnav=1&wvr=6&b=1',
 ];
