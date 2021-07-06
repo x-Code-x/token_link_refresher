17dc6d39267f92446bbf2c68b6ffae21a043fa36
Henry Choo <lchoo9294@gmail.com>
2021-07-07 00:51:46 +0800

    refactor: added more links

--- src/links.ts
+++ src/links.ts
@@ -3,4 +3,9 @@ export const websiteLinks = [
   'https://coinmarketcap.com/currencies/everrise/',
   'https://www.coingecko.com/en/coins/everrise',
   'https://s.weibo.com/weibo/everrise?topnav=1&wvr=6&b=1',
+  'https://bscscan.com/token/0xc7d43f2b51f44f09fbb8a691a0451e8ffcf36c0a',
+  'https://www.reddit.com/r/EverRise/',
+  'https://www.google.com/search?q=everrisecoin&sxsrf=ALeKk01uSlux_sxg8w7xtvRptJAfq8HYCA%3A1625590197918&ei=tYnkYOC7N5_hz7sPs56D0As&oq=everrisecoin&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEEMyBAgAEAoyBAgAEAoyBAgAEAoyBAgAEAoyBAgAEAoyBAgAEAoyBAgAEAo6BwgjELADECc6BwgAEEcQsAM6EAguEMcBEK8BELADEMgDEEM6CgguEMcBEK8BEAo6BwgAEMkDEApKBQg4EgExSgQIQRgAUNp4WIJ6YMN7aAFwAngBgAHbAYgBqwSSAQUzLjEuMZgBAKABAaoBB2d3cy13aXrIAQzAAQE&sclient=gws-wiz&ved=0ahUKEwig6q-y887xAhWf8HMBHTPPALoQ4dUDCA4&uact=5',
+  'https://www.google.com/search?q=everrise&sxsrf=ALeKk002i6ri6A96EVvu8u7GngM6p3Mq_g%3A1625590226192&ei=0onkYMSUC8Hbz7sPxIubyAw&oq=everrise&gs_lcp=Cgdnd3Mtd2l6EAMyBwgjELADECcyBwgjELADECcyBwgjELADECcyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyEAguEMcBEK8BELADEMgDEEMyEAguEMcBEK8BELADEMgDEEMyEAguEMcBEK8BELADEMgDEENKBQg4EgExSgQIQRgAUABYAGCJY2gCcAJ4AIABc4gBc5IBAzAuMZgBAKoBB2d3cy13aXrIAQzAAQE&sclient=gws-wiz&ved=0ahUKEwjExe2_887xAhXB7XMBHcTFBskQ4dUDCA4&uact=5',
+  'https://twitter.com/EverRiseToken',
 ];
